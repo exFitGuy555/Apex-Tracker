5ff280a3011d99da227d3bf674957d1eb38e38e8
exFitGuy555 <guy.finkelshtein5@gmail.com>
2020-08-05 18:20:03 +0300

    Initial commit

--- server.js
+++ server.js
@@ -11,21 +11,21 @@ const app = express()
 
 //Dev wrapWithLogging
 //Extra detailed log
-if (process.env.NODE_ENV === 'development'){
+if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
 }
 
 //Profile Routes
-app.use('/api/v1/profile', require('./routes/profile'))ף
+app.use('/api/v1/profile', require('./routes/profile'))
 
 
 //handle production
-if(process.env.NODE_ENV === 'production'){
+if (process.env.NODE_ENV === 'production') {
     //Set static Folder
     app.use(express.static(__dirname + '/public/'))
 
     //Handle SPA - any req thats not a part of our API
-    app.get(/.*/, (req,res) => res.sendFile(__dirname + '/public/index.html'))
+    app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'))
 }
 
 const port = process.env.PORT || 8000;
